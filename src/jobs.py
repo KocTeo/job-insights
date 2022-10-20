ed057a0cedffb749bc97aa1f3da34f005abbdbbd
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-10-20 16:07:31 -0300

    docs: Retirando comentários do arquivo

--- src/jobs.py
+++ src/jobs.py
@@ -3,27 +3,11 @@ import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-
-
     try:
         with open(path, encoding="utf-8") as file:
             jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
 
-            jobs = [job for job in jobs_reader]
-
-            return jobs
+            return [job for job in jobs_reader]
 
 
     except FileNotFoundError as exc:
