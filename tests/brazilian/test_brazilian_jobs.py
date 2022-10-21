d2530564553331108ef015a06c04c577266fc86f
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2022-10-21 15:56:28 -0300

    test: função que testa a tradução das chaves dos jobs que estão em pt-br

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    keys_expect = {("title", "salary", "type")}
+
+    job_keys = set()
+
+    for job in jobs:
+        job_keys.add(tuple(job.keys()))
+
+    assert keys_expect == job_keys
